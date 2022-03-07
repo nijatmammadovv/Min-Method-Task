3b0c1a0c9d622bc46bd87cbb25bebf171900854a
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-07 22:02:08 +0400

    Add project files.

---
+++ Min Method Task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Min_Method_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 22, 33, 42345, 543, 65, 7, -34, 0, -873 };
+            Min(array);
+            Console.WriteLine(Min(array));
+        }
+
+        static int Min(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+    }
+}
